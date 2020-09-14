883ae171c6248ba1f2f169a5160045e9db12591d
Lang Luo <langluo9843@gmail.com>
2020-09-13 21:03:08 -0700

    complete

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+var circles = [];
+var keyData = {
+  a: { color: "#BD93F9", sound: "bubbles" },
+  b: { color: "#50FA7B", sound: "clay" },
+  c: { color: "#F9D368", sound: "confetti" },
+  d: { color: "#8ADEEF", sound: "corona" },
+  e: { color: "#E87BC2", sound: "flash-1" },
+  f: { color: "#EDEDED", sound: "dotted-spiral" },
+  g: { color: "#35AF94", sound: "glimmer" },
+  h: { color: "#FA877A", sound: "moon" },
+  i: { color: "#E0554A", sound: "pinwheel" },
+  j: { color: "#FFE473", sound: "piston-1" },
+  k: { color: "#B06428", sound: "piston-2" },
+  l: { color: "#FF5252", sound: "piston-3" },
+  m: { color: "#CCCCCE", sound: "prism-1" },
+  n: { color: "#33B5C6", sound: "prism-2" },
+  o: { color: "#52C3A3", sound: "prism-3" },
+  p: { color: "#5E73DD", sound: "splits" },
+  q: { color: "#5AFFB7", sound: "squiggle" },
+  r: { color: "#FDED32", sound: "strike" },
+  s: { color: "#D587EB", sound: "squiggle" },
+  t: { color: "#50BC69", sound: "timer" },
+  u: { color: "#E87BC2", sound: "ufo" },
+  v: { color: "#75A5D3", sound: "veil" },
+  w: { color: "#FC5426", sound: "wipe" },
+  z: { color: "#B0DDE9", sound: "zig-zag" },
+  y: { color: "#A72975", sound: "flash-2" },
+  z: { color: "#4B68D1", sound: "flash-3" },
+  shift: { color: "#F9D368", sound: "bubbles" },
+  control: { color: "#BD93F9", sound: "squiggle" },
+  space: { color: "#F9D368", sound: "corona" },
+  tab: { color: "#50FA7B", sound: "clay" },
+};
+var sound;
+
+function onKeyDown(event) {
+  // sound ? sound.stop() : null;
+  // get key color and sound using keyData object
+  var color = keyData[event.key] ? keyData[event.key].color : "#50FA7B";
+  var soundSrc = keyData[event.key]
+    ? "assets/sounds/" + keyData[event.key].sound + ".mp3"
+    : "assets/sounds/bubbles.mp3";
+  sound = new Howl({
+    src: [soundSrc],
+  });
+  // generate a random point on canvas when a key is pressed
+  var maxPoint = new Point(view.size.width, view.size.height);
+  var randomPoint = Point.random();
+  var point = maxPoint * randomPoint;
+  var newCircle = new Path.Circle(point, 500);
+  newCircle.fillColor = color;
+  circles.push(newCircle);
+  // play sound
+  sound.play();
+}
+
+function onFrame(event) {
+  // animate hue, size of each circle in circles array
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].fillColor.hue += 1;
+    circles[i].scale(0.9);
+    if (circles[i].area < 1) {
+      circles[i].remove(); // remove the circle from the canvas
+      circles.splice(i, 1); // remove the circle from the array
+    }
+  }
+}
